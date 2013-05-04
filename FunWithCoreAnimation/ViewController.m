3df65b1652adb7a81c9428ade08f49cc4ae35ba6
Ben Ford <bentford@gmail.com>
2013-05-04 13:30:48 -0700

    added y value

--- FunWithCoreAnimation/ViewController.m
+++ FunWithCoreAnimation/ViewController.m
@@ -36,13 +36,29 @@
 - (void)toggleView:(UIView *)view
 {
     if (view.layer.position.x == 50.0f) {
-        CAKeyframeAnimation *keyFrameAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position.x"];
-        keyFrameAnimation.values = @[ @50.0, @200.0 ];
-        keyFrameAnimation.keyTimes = @[ @0.0, @1.0 ];
-        keyFrameAnimation.duration = 0.3f;
-
-        [view.layer addAnimation:keyFrameAnimation forKey:@"go"];
-        view.layer.position = CGPointMake(200.0f, 50.0f);
+        
+        CAAnimationGroup *group = [CAAnimationGroup animation];
+        
+        CAKeyframeAnimation *xMovement = [CAKeyframeAnimation animationWithKeyPath:@"position.x"];
+        xMovement.values = @[ @50.0, @200.0 ];
+        xMovement.keyTimes = @[ @0.0, @1.0 ];
+        xMovement.duration = 0.3f;
+        xMovement.calculationMode = @"cubic";
+        
+        CAKeyframeAnimation *yMovement = [CAKeyframeAnimation animationWithKeyPath:@"position.y"];
+        yMovement.values = @[ @50.0, @200.0 ];
+        yMovement.keyTimes = @[ @0.0, @1.0 ];
+        yMovement.duration = 0.3f;
+        yMovement.calculationMode = @"cubic";
+        
+        
+        group.animations = @[xMovement,yMovement];
+        
+        [view.layer addAnimation:group forKey:@"go"];
+        
+        
+        view.layer.position = CGPointMake(200.0f, 200.0f);
+        
     } else {
         CAKeyframeAnimation *keyFrameAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position.x"];
         keyFrameAnimation.values = @[ @200.0, @50.0 ];
